22ab1f3d499529850868ea58366fae8f2d8c22a2
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-02-13 20:51:50 +0100

    database connection

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const
+const express = require("express");
+require("dotenv").config();
+const {connectToMongodb} = require("./database");
+
+const app = express();
+connectToMongodb();
+
+const PORT = 5000;
+
+
+app.use(express.json());
+
+app.use((err, req, res, next)=>{
+    console.log(err);
+    res.status(400).send(err.message)
+    next()
+})
+
+app.get("/", (req, res)=>{
+    res.status(200).send("Home Page!!")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening at ${PORT}`)
+})
